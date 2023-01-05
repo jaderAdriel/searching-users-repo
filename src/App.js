567eaf37454d15e3b0ea452bd4fc479dd10165c6
jaderAdriel <jaderadriel7@gmail.com>
2023-01-05 17:13:37 -0300

    :bug: fix: show error before search

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Repo from './components/Repo';
 import './App.css';
 
 function App() {
-  const [user, setUser] = useState("jaderAdriel");
+  const [user, setUser] = useState('');
   const [state, setState] = useState({
     name:"sucess", message: null
   });
@@ -13,7 +13,7 @@ function App() {
   return (
     <div className="App">
         <Form setUser={setUser} user={user}/>
-        <State state={state}/>
+        { user ? <State state={state}/> : ''}
         <Repo state={state} setState={setState} user={user}/>
     </div>
   );
